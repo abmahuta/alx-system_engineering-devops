59d63045566751bb65aff548f4e96b3ba651788f
Abdulkadir <mahuta2012@gmail.com>
2022-12-09 13:31:11 -0800

    error fix

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+# puppet manifest that installs puppet-lint
+
+package {'puppet-lint':
+  ensure => '2.5.0'
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,5 @@
+# This manifest kills a process named killmenow.
+
+exec {'killmenow':
+  command => '/usr/bin/pkill -TERM killmenow',
+}
