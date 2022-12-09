d8f46825bb93f9a149d259cb8753c78274c757a9
Abdulkadir <mahuta2012@gmail.com>
2022-12-09 13:42:13 -0800

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-#Puppet manifest that installs puppet-lint
+#Puppet manifest that installs flask version 2.1.0
 
-package {'puppet-lint':
-  ensure => '2.5.0'
+package {'flask':
+  ensure => '2.1.0',
+  provider => 'pip3'
 }
