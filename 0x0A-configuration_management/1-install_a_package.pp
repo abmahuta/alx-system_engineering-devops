0cf84e27cf7bdd4c7a0c4df566cafc9adc715e9a
Abdulkadir <mahuta2012@gmail.com>
2022-12-09 13:52:23 -0800

    error fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/puppet
-# Puppet manifest that installs flask version 2.1.0
-
+# Installs flask version 2.1.0
 package {'flask':
-  ensure => '2.1.0',
+  ensure   => '2.1.0',
   provider => 'pip3'
 }
